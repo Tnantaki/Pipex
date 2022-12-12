78865b700cb60fd69b2a2288f6f1995f067703a6
Tanin Nantakitgosol <tnantaki@z2t5c1.42bangkok.com>
2022-12-12 19:52:57 +0700

    by alias

--- learn/exec.c
+++ learn/exec.c
@@ -1,10 +1,28 @@
-#include <unistd.h>
+#include "../ft_pipex.h"
 
-int main(void) {
-  char *programName = "ls";
-  char *arg1 = "-la";
-
-  execlp(programName, arg1, NULL);
-
-  return 0;
+int main(void)
+{
+	int fd[2];
+	if (pipe(fd) == -1) return (1);
+	int pid = fork();
+	if (pid == -1) return (1);
+	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		execlp("ls", "ls", "-la", ".", NULL);
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		execlp("grep", "grep", "file", NULL);
+	}
+	close(fd[1]);
+	close(fd[0]);
+	wait(NULL);
+	printf("Hello\n");
 }
