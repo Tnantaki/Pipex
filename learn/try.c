dc19277fea82b3179f5e1c6cc5c48deb9c7dc98b
Tanin Nantakitgosol <tnantaki@z1t6c1.42bangkok.com>
2023-02-04 20:06:49 +0700

    by alias

--- learn/try.c
+++ learn/try.c
@@ -6,12 +6,25 @@
 
 int	main(void)
 {
-	int fd = open("outfile", O_RDWR | O_CREAT | O_TRUNC, 0777);
-	printf("fd:%d\n", fd);
-	unlink("outfile");
-	printf("fd:%d\n", fd);
-	write(fd, "hello\n", 6);
+	// int fd = open("outfile", O_RDWR | O_CREAT | O_TRUNC, 0777);
+	// printf("fd:%d\n", fd);
+	// unlink("outfile");
+	// printf("fd:%d\n", fd);
+	// write(fd, "hello\n", 6);
 	// int fd2 = open("infile", O_RDWR);
 	// printf("fd:%d\n", fd2);
-
+	int pid = fork();
+	if (pid == 0)
+	{
+		sleep (4);
+		printf("Child\n");
+	}
+	else
+	{
+		wait(NULL);
+		// execve();
+		sleep (5);
+		printf("Parent\n");
+	}
+	// wait(NULL);
 }
